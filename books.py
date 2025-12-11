c4de20c0338b43a2571c3590b07fd721dc00e571
saksham406 <sakshamsinha406@gmail.com>
2025-12-11 19:51:59 +0530

    fast_api

---
+++ books.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI() # allow to use fastapi pacakges
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+
+@app.get("/books") #adding decorator to call this function
+async def read_all_books(): # asycn is not needed in fastapi because it will asycn behine scenes
+    return BOOKS
