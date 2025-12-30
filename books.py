ff2dbc388f730a88d6b8d9b629d37b2ab1bceb2b
saksham406 <sakshamsinha406@gmail.com>
2025-12-31 00:14:45 +0530

    path parameters

--- books.py
+++ books.py
@@ -11,7 +11,19 @@ BOOKS = [
     {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
 ]
 
-
-@app.get("/books") #adding decorator to call this function
-async def read_all_books(): # asycn is not needed in fastapi because it will asycn behine scenes
+@app.get("/books/read_all_books/")
+async def read_all_books():
     return BOOKS
+
+
+@app.get("/books/{book_title}/") #adding decorator to call this function
+async def read_book(book_title: str): # asycn is not needed in fastapi because it will asycn behine scenes
+    for book in BOOKS:
+        if book['title'].lower() == book_title.lower():
+            return book
+
+@app.get("/books/my_books/")
+async def get_my_favourite_book():
+    return {
+        'my books': 'My Favourite Book'
+    }
